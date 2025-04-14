0ab23a0e3416e199b42298f280c506ddc9662d10
Dracudar <dracudar@gmail.com>
2025-04-14 23:41:31 +0200

    Modification de la fonction main pour utiliser menu_selection au lieu de menu_initialisation (modification du nom dans front.py)

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ Point d'entrée de l'application SnackApp
 '''
 
 # Importer la fonction menu_initialisation depuis front.py
-from front import menu_initialisation
+from front import menu_selection
 
-def main():
-    '''
-    Point d'entrée principal de l'application.
-    '''
-    # Lancer le menu d'initialisation
-    menu_initialisation()
+def main(): # Point d'entrée principal de l'application.
+    menu_selection()
 
 # Vérifier si le script est exécuté directement
 if __name__ == "__main__":
