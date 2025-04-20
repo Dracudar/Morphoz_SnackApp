256bef62c9ea8599e4033615a146bb20851b5f18
Dracudar <dracudar@gmail.com>
2025-04-20 18:56:06 +0200

    Suppression des importations inutiles et nettoyage du code dans le point d'entrée de l'application.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# filepath: d:\Projets\Programmation\Morphoz_SnackApp\main.py
-import sys
-import os
-
-# Ajouter le dossier 'src' au chemin Python
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-
 '''
 Point d'entrée de l'application SnackApp
 '''
@@ -12,7 +5,7 @@ Point d'entrée de l'application SnackApp
 # Importer la fonction menu_initialisation depuis front.py
 from src.front import menu_selection
 
-def main():  # Point d'entrée principal de l'application.
+def main(): # Point d'entrée principal de l'application.
     menu_selection()
 
 # Vérifier si le script est exécuté directement
