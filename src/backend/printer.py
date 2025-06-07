52a0d78ab3aebbab0a4894248120e92c826ddf59
Dracudar <dracudar@gmail.com>
2025-06-07 22:56:51 +0200

    Ajout d'une gestion pour imprimer le détail des plats "En attente" uniquement

--- src/backend/printer.py
+++ src/backend/printer.py
@@ -6,14 +6,14 @@ import json
 import os
 from PIL import Image
 from escpos.printer import Usb
+from .commandes_utils import charger_fichier_commande
 
-def charger_fichier_commande(chemin_fichier):
-    """Charge un fichier JSON de commande."""
-    if not os.path.exists(chemin_fichier):
-        raise FileNotFoundError(f"Fichier commande introuvable : {chemin_fichier}")
-    with open(chemin_fichier, "r", encoding="utf-8") as f:
-        return json.load(f)
+# Paramètres de configuration de l'imprimante thermique
+IMPRIMANTE_USB_VENDOR_ID = 0x04B8
+IMPRIMANTE_USB_PRODUCT_ID = 0x0E15
+IMPRIMANTE_USB_INTERFACE = 0
 
+# Fonction pour charger une image et la redimensionner pour l'impression thermique
 def charger_logo(nom_image, taille=()):
     """
     Charge et redimensionne une image PIL pour l'impression thermique.
@@ -38,19 +38,11 @@ def charger_logo(nom_image, taille=()):
     except Exception as e:
         raise RuntimeError(f"Erreur lors du chargement de l'image '{nom_image}': {e}")
 
-
-def en_tete(p):
-    logo = charger_logo("En-tete ticket V1.png", taille=(500, 107))  # Respecte le ratio 7:1.5
-    # Centrer et imprimer le logo
-    p.set(align='center')
-    p.image(logo)
-    p.set(align='left')
-
 def print_ticket_recap(chemin_fichier):
     """Imprime un ticket récapitulatif de la commande."""
     # Charger l'imprimante USB
     try:
-        p = Usb(0x04B8, 0x0E15, 0)
+        p = Usb(IMPRIMANTE_USB_VENDOR_ID, IMPRIMANTE_USB_PRODUCT_ID, IMPRIMANTE_USB_INTERFACE)
 
         # Charger la commande depuis le fichier JSON
         commande = charger_fichier_commande(chemin_fichier)
@@ -58,7 +50,10 @@ def print_ticket_recap(chemin_fichier):
         plats = commande["Commande"]
 
         # En-tête
-        en_tete(p)
+        logo = charger_logo("En-tete ticket V1.png", taille=(500, 107))  # Respecte le ratio 7:1.5
+        p.set(align='center')
+        p.image(logo)
+        p.set(align='left')
         # Informations de la commande
         p.set(align='center', width=2, height=2)
         p.text(f"Date : {infos['Date de création'][0]} \n")  # Affiche la date de création (sans l'heure)
@@ -70,74 +65,69 @@ def print_ticket_recap(chemin_fichier):
 
         # Parcours des plats de la commande
         for plat_id, plat in plats.items():
-            # Personnalisation selon le type de plat
-            if plat["Plat"] == "Frites" :
+            if plat["Statut"] == "En attente":  # Imprime uniquement les plats en attente
                 p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Nom']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                # Personnalisation selon le type de plat
+                if plat["Plat"] == "Frites" :
+                    p.text(f"{plat_id} - {plat['Nom']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
 
-            elif plat["Plat"] == "Grillade" :
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                elif plat["Plat"] == "Grillade" :
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
 
-                if isinstance(plat['Composition']['Viandes'], dict):
-                    if len(plat['Composition']['Viandes']) == 1:
-                        nom_viande, quantite = next(iter(plat['Composition']['Viandes'].items()))
-                        if quantite > 1:
-                            p.text(f"Viandes : {nom_viande} x{quantite}\n")
-                        else:
-                            p.text(f"Viandes : {nom_viande}\n")
-                    elif len(plat['Composition']['Viandes']) > 1:
-                        p.text("Viandes :\n")
-                        for nom_viande, quantite in plat['Composition']['Viandes'].items():
+                    if isinstance(plat['Composition']['Viandes'], dict):
+                        if len(plat['Composition']['Viandes']) == 1:
+                            nom_viande, quantite = next(iter(plat['Composition']['Viandes'].items()))
                             if quantite > 1:
-                                p.text(f"  - {nom_viande} x{quantite}\n")
+                                p.text(f"Viandes : {nom_viande} x{quantite}\n")
                             else:
-                                p.text(f"  - {nom_viande}\n")
-                if plat['Composition']['Accompagnement'] == "Sans" :
-                    p.text("Sans accompagnement\n")
-                else:    
-                    p.text(f"Accompagnement : {plat['Composition']['Accompagnement']}\n")
+                                p.text(f"Viandes : {nom_viande}\n")
+                        elif len(plat['Composition']['Viandes']) > 1:
+                            p.text("Viandes :\n")
+                            for nom_viande, quantite in plat['Composition']['Viandes'].items():
+                                if quantite > 1:
+                                    p.text(f"  - {nom_viande} x{quantite}\n")
+                                else:
+                                    p.text(f"  - {nom_viande}\n")
+                    if plat['Composition']['Accompagnement'] == "Sans" :
+                        p.text("Sans accompagnement\n")
+                    else:    
+                        p.text(f"Accompagnement : {plat['Composition']['Accompagnement']}\n")
 
-            elif plat["Plat"] == "Pizza" :
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                # p.text(f"{plat['Recette']}\n")  # TODO : voir pour enregistrer la recette de pizza choisie dans le fichier de commande
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
-                p.text('\n')
-                p.text(f"Base : {plat['Composition']['Base']}\n")
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']['Ingrédients']:
-                    p.text(f"  - {ingredient}\n")
-            
-            elif plat["Plat"] == "Fish & Chips" :
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                elif plat["Plat"] == "Pizza" :
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    # p.text(f"{plat['Recette']}\n")  # TODO : voir pour enregistrer la recette de pizza choisie dans le fichier de commande
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                    p.text('\n')
+                    p.text(f"Base : {plat['Composition']['Base']}\n")
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']['Ingrédients']:
+                        p.text(f"  - {ingredient}\n")
+                
+                elif plat["Plat"] == "Fish & Chips" :
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
 
-            elif plat["Plat"] == "Salade composée" :
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
-                p.text('\n')
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']['Ingrédients']:
-                    p.text(f"  - {ingredient}\n")
+                elif plat["Plat"] == "Salade composée" :
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                    p.text('\n')
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']['Ingrédients']:
+                        p.text(f"  - {ingredient}\n")
 
-            elif plat["Plat"] == "Pizza dessert" :
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
-                p.text('\n')
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']:
-                    p.text(f"  - {ingredient}\n")
-            
-            else:
-                p.text("-" * 48 + "\n")  # Ligne de séparation
-                p.text(f"{plat_id} - {plat['Plat']}\n")
-                p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                elif plat["Plat"] == "Pizza dessert" :
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
+                    p.text('\n')
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']:
+                        p.text(f"  - {ingredient}\n")
+                
+                else:
+                    p.text(f"{plat_id} - {plat['Plat']}\n")
+                    p.text(f"Prix : {plat['Prix']} €\n")  # Affiche le prix du plat
 
         p.cut()
         p.close()
@@ -151,7 +141,7 @@ def print_ticket_cuisine(chemin_fichier):
     
     # Charger l'imprimante USB
     try:
-        p = Usb(0x04B8, 0x0E15, 0)
+        p = Usb(IMPRIMANTE_USB_VENDOR_ID, IMPRIMANTE_USB_PRODUCT_ID, IMPRIMANTE_USB_INTERFACE)
 
         # Charger la commande depuis le fichier JSON    
         commande = charger_fichier_commande(chemin_fichier)
@@ -160,87 +150,86 @@ def print_ticket_cuisine(chemin_fichier):
 
         # Parcours des plats de la commande
         for plat_id, plat in plats.items():
-            # Extraction de l'ID plat pour l'affichage
-            id_parts = str(plat['ID']).split('-')
-            if len(id_parts) >= 3:
-                id_affiche = f"{id_parts[1]}-{id_parts[2]}"
-            else:
-                id_affiche = str(plat['ID'])  # fallback si le format ne correspond pas
+            if plat["Statut"] == "En attente":  # Imprime uniquement les plats en attente
 
-            # En-tête du ticket cuisine (date et ID plat 000-00)
-            p.set(align='center', width=2, height=2)
-            p.text(f"Date : {infos['Date de création'][0]} \n")  # Affiche la date de création (sans l'heure)
-            p.set(width=8, height=8)
-            p.text(f"{id_affiche}\n")  # Affiche le 000-00
-            p.set(align='left', width=1, height=1)
+                # Extraction de l'ID plat pour l'affichage
+                id_parts = str(plat['ID']).split('-')
+                if len(id_parts) >= 3:
+                    id_affiche = f"{id_parts[1]}-{id_parts[2]}"
+                else:
+                    id_affiche = str(plat['ID'])  # fallback si le format ne correspond pas
 
-            # Personnalisation selon le type de plat
-            if plat["Plat"] == "Frites" :
-                p.text(f"{plat['Nom']}\n")
-                p.text("\n" * 5)  # Ligne vide
+                # En-tête du ticket cuisine (date et ID plat 000-00)
+                p.set(align='center', width=2, height=2)
+                p.text(f"Date : {infos['Date de création'][0]} \n")  # Affiche la date de création (sans l'heure)
+                p.set(width=8, height=8)
+                p.text(f"{id_affiche}\n")  # Affiche le 000-00
+                p.set(align='left', width=1, height=1)
 
-            elif plat["Plat"] == "Grillade" :
-                p.text(f"{plat['Plat']}\n")
+                # Personnalisation selon le type de plat
+                if plat["Plat"] == "Frites" :
+                    p.text(f"{plat['Nom']}\n")
+                    p.text("\n" * 5)  # Ligne vide
 
-                if isinstance(plat['Composition']['Viandes'], dict):
-                    if len(plat['Composition']['Viandes']) == 1:
-                        nom_viande, quantite = next(iter(plat['Composition']['Viandes'].items()))
-                        if quantite > 1:
-                            p.text(f"Viandes : {nom_viande} x{quantite}\n")
-                        else:
-                            p.text(f"Viandes : {nom_viande}\n")
-                    elif len(plat['Composition']['Viandes']) > 1:
-                        p.text("Viandes :\n")
-                        for nom_viande, quantite in plat['Composition']['Viandes'].items():
+                elif plat["Plat"] == "Grillade" :
+                    p.text(f"{plat['Plat']}\n")
+
+                    if isinstance(plat['Composition']['Viandes'], dict):
+                        if len(plat['Composition']['Viandes']) == 1:
+                            nom_viande, quantite = next(iter(plat['Composition']['Viandes'].items()))
                             if quantite > 1:
-                                p.text(f"  - {nom_viande} x{quantite}\n")
+                                p.text(f"Viandes : {nom_viande} x{quantite}\n")
                             else:
-                                p.text(f"  - {nom_viande}\n")
-                if plat['Composition']['Accompagnement'] == "Sans" :
-                    p.text("Sans accompagnement\n")
-                else:    
-                    p.text(f"Accompagnement : {plat['Composition']['Accompagnement']}\n")
+                                p.text(f"Viandes : {nom_viande}\n")
+                        elif len(plat['Composition']['Viandes']) > 1:
+                            p.text("Viandes :\n")
+                            for nom_viande, quantite in plat['Composition']['Viandes'].items():
+                                if quantite > 1:
+                                    p.text(f"  - {nom_viande} x{quantite}\n")
+                                else:
+                                    p.text(f"  - {nom_viande}\n")
+                    if plat['Composition']['Accompagnement'] == "Sans" :
+                        p.text("Sans accompagnement\n")
+                    else:    
+                        p.text(f"Accompagnement : {plat['Composition']['Accompagnement']}\n")
 
-            elif plat["Plat"] == "Pizza" :
-                p.text(f"{plat['Plat']}\n")
-                # p.text(f"{plat['Recette']}\n")  # TODO : voir pour enregistrer la recette de pizza choisie dans le fichier de commande
-                p.text("\n") # Ligne vide
-                p.text(f"Base : {plat['Composition']['Base']}\n")
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']['Ingrédients']:
-                    p.text(f"  - {ingredient}\n")
-            
-            elif plat["Plat"] == "Fish & Chips" :
-                p.text(f"{plat['Plat']}\n")
-                p.text("\n") # Ligne vide
-                p.text("Composition :\n")
-                p.text("  - Poisson pané\n")
-                p.text("  - Portion de frites\n")
+                elif plat["Plat"] == "Pizza" :
+                    p.text(f"{plat['Plat']}\n")
+                    # p.text(f"{plat['Recette']}\n")  # TODO : voir pour enregistrer la recette de pizza choisie dans le fichier de commande
+                    p.text("\n") # Ligne vide
+                    p.text(f"Base : {plat['Composition']['Base']}\n")
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']['Ingrédients']:
+                        p.text(f"  - {ingredient}\n")
+                
+                elif plat["Plat"] == "Fish & Chips" :
+                    p.text(f"{plat['Plat']}\n")
+                    p.text("\n") # Ligne vide
+                    p.text("Composition :\n")
+                    p.text("  - Poisson pané\n")
+                    p.text("  - Portion de frites\n")
 
-            elif plat["Plat"] == "Salade composée" :
-                p.text(f"{plat['Plat']}\n")
-                p.text("\n") # Ligne vide
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']['Ingrédients']:
-                    p.text(f"  - {ingredient}\n")
+                elif plat["Plat"] == "Salade composée" :
+                    p.text(f"{plat['Plat']}\n")
+                    p.text("\n") # Ligne vide
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']['Ingrédients']:
+                        p.text(f"  - {ingredient}\n")
 
-            elif plat["Plat"] == "Pizza dessert" :
-                p.text(f"{plat['Plat']}\n")
-                p.text("\n") # Ligne vide
-                p.text("Composition :\n")
-                for ingredient in plat['Composition']:
-                    p.text(f"  - {ingredient}\n")
-            
-            else:
-                p.text(f"{plat['Plat']}\n")
+                elif plat["Plat"] == "Pizza dessert" :
+                    p.text(f"{plat['Plat']}\n")
+                    p.text("\n") # Ligne vide
+                    p.text("Composition :\n")
+                    for ingredient in plat['Composition']:
+                        p.text(f"  - {ingredient}\n")
+                
+                else:
+                    p.text(f"{plat['Plat']}\n")
 
-            p.text("\n") # Ligne vide
-            p.cut()
-            p.close()
+                p.text("\n") # Ligne vide
+                p.cut()
+                p.close()
 
     except Exception as e:
         print(f"Imprimante non disponible : {e}")
         return  # Arrête la fonction si l'imprimante n'est pas disponible
-
-    
-
